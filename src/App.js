65d167c7dbe148f117c10923113c0544b4c74a6b
unknown <szymon.gawlowski@vp.pl>
2022-11-17 22:23:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import AddItem from './AddItem/AddItem';
+import Shop from './Shop';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Item from './ItemDetail';
+import Home from './Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/additem" element={<AddItem />} />
+          <Route path="/shop" exact element={<Shop />} />
+          <Route path="/shop/:id" element={<Item/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+
 
 export default App;
